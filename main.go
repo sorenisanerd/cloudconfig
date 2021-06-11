6785c4a5e107b9bfd31a9000d6efc781284d5c96
Søren L. Hansen <soren.hansen@mastercard.com>
2021-06-11 20:45:06 +0100

    Initial import

---
+++ main.go
@@ -0,0 +1,61 @@
+package cloudconfig
+
+import (
+	yaml "gopkg.in/yaml.v3"
+)
+
+type CloudConfig struct {
+	Password  *string   `yaml:",omitempty"`
+	Packages  []string  `yaml:",omitempty"`
+	SshPwAuth *bool     `yaml:"ssh_pwauth,omitempty"`
+	ChPasswd  *ChPasswd `yaml:",omitempty"`
+	Commands  []Command `yaml:",flow,omitempty"`
+	Mounts    []Mount   `yaml:",omitempty,flow"`
+}
+
+type Mount []string
+type Command []string
+
+type ChPasswd struct {
+	Expire bool
+}
+
+type CloudConfigValue interface{}
+
+func New() *CloudConfig {
+	return &CloudConfig{}
+}
+
+func (cc *CloudConfig) AddPackage(pkgname string) {
+	cc.Packages = append(cc.Packages, pkgname)
+}
+
+// AddMount adds a mount entry (like a line in /etc/fstab).
+// First argument is `fs_spec`. It describes the block special device
+// or remote filesystem to be mounted.
+// Second argument is `fs_file`. It describes the mount point.
+// An optional third argument is filesystem type.
+// An optional fourth argument is mount options (noatime and such).
+// An optional fifth argument tells dump(8) is mount options (noatime and such).
+// An optional sixth argument is passno. It determines the order in which
+// fsck is run at boot.
+func (cc *CloudConfig) AddMount(device, mountPoint string, moreInfo ...string) {
+	cc.Mounts = append(cc.Mounts, append([]string{device, mountPoint}, moreInfo...))
+}
+
+func (cc *CloudConfig) AddRunCmd(cmdArgs ...string) {
+	cc.Commands = append(cc.Commands, cmdArgs)
+}
+
+func (cc *CloudConfig) SetPassword(password string) {
+	cc.Password = &password
+}
+
+func (cc *CloudConfig) SetSshPwAuth(enable bool) {
+	cc.SshPwAuth = &enable
+}
+
+func (cc *CloudConfig) Generate() ([]byte, error) {
+	out, _ := yaml.Marshal(cc)
+	return append([]byte("#cloud-config\n"), out...), nil
+}
---
+++ main_test.go
@@ -0,0 +1,41 @@
+package cloudconfig
+
+import (
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+)
+
+func TestCloudConfig(t *testing.T) {
+	cc := New()
+	cc.SetPassword("passw0rd")
+	cc.SetSshPwAuth(true)
+	cc.AddPackage("nfs-client")
+	cc.AddPackage("nfs-common")
+	cc.AddMount("1.2.3.4:/foobar", "/shared", "nfs")
+	cc.AddMount("/dev/sda1", "/data", "ext4", "noatime,noexec")
+	cc.AddRunCmd("mount", "-a")
+	expected := `#cloud-config
+password: passw0rd
+packages:
+    - nfs-client
+    - nfs-common
+ssh_pwauth: true
+commands: [[mount, -a]]
+mounts: [['1.2.3.4:/foobar', /shared, nfs], [/dev/sda1, /data, ext4, 'noatime,noexec']]
+`
+	check(cc, expected, t)
+}
+
+func check(cc *CloudConfig, expected string, t *testing.T) {
+	got, _ := cc.Generate()
+	s := string(got)
+	if diff := cmp.Diff(expected, s); diff != "" {
+		t.Errorf("cc.Generated() mismatch (-want +got):\n%s", diff)
+	}
+}
+
+func TestCloudConfigEmpty(t *testing.T) {
+	cc := New()
+	check(cc, "#cloud-config\n{}\n", t)
+}
